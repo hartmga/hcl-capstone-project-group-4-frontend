44467f399dfbc1075d5e583faaa715ec54268d4a
Gabriel Hartmark <gabriel_hartmark@hcl.com>
2021-08-25 17:39:40 -0400

    initial project structure

--- src/App.js
+++ src/App.js
@@ -1,58 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
-  );
+  return (<>
+      <Router>
+        {/* a topbar could go here */}
+        <Switch>
+          <Route exact path="/">
+            homepage
+          </Route>
+          <Route exact path="/login">
+            login form goes here
+          </Route>
+          <Route exact path="/register">
+            registration form goes here
+          </Route>
+          <Route path="/products">
+            product catalog could go here
+          </Route>
+          <Route path="/admin">
+            all admin routes can be nested here
+          </Route>
+        </Switch>
+      </Router>
+  </>);
 }
 
 export default App;
---
+++ src/redux/cartSlice.js
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+
+    },
+    reducers: {
+        //TODO: create reducers
+    },
+
+});
+
+export default cartSlice.reducer;
